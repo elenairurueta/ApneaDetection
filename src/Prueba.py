09c9dee4bc37907b1fb4bf5dfced435f179b7673
Elena <elena@irurueta.com.ar>
2024-06-26 12:37:19 -0300

    Update Prueba.py

--- src/Prueba.py
+++ src/Prueba.py
@@ -1,10 +1,10 @@
-#Import the required modules:
 from DataFormatting import ApneaDataset
 from Modelo import *
 from Training import Trainer
 from Testing import Tester
 from LecturaSenalesReales import *
 from LecturaAnotaciones import *
+from PlotSignals import *
 
 archivo = 63
 txt_archivo = f"homepap-lab-full-1600{str(archivo).zfill(3)}\n"
@@ -15,26 +15,31 @@ annotations = Anotaciones(path_annot)
 
 
 bipolar_signal, tiempo, sampling = get_bipolar_signal(all_signals['C3'], all_signals['O1'])
+
+plot_signals(annotations, tiempo, 
+             all_signals['C3'], 'C3', 
+             all_signals['O1'], 'O1', 
+             bipolar_signal, 'C3-O1')
+
 segments = get_signal_segments_strict(bipolar_signal, tiempo, sampling, annotations)
 X,y = ApneaDataset.from_segments(segments)
 dataset = ApneaDataset(X,y, archivo)
 
-#Get signal length
+
 input_size = dataset.signal_len()
-#Split the Dataset into train, validation and test Subsets
+
 dataset.split_dataset(train_perc = 0.6, 
                       val_perc = 0.2, 
                       test_perc = 0.2)
-#Data statistics
+
 analisis_datos = dataset.analisis_datos()
 print(analisis_datos)
 dataset.undersample_majority_class(0.0)
 analisis_datos = dataset.analisis_datos()
 print(analisis_datos)
 
-nombre = 'modelo_senalesreales' #CHANGE, name used to save the model and figures
+nombre = 'modelo_senalesreales'
 
-#Create and train new model
 model = Model1(input_size, nombre)
 model_arch = model.get_architecture()
 trainer = Trainer(
@@ -49,10 +54,10 @@ trainer = Trainer(
     momentum = 0.5, 
     text = txt_archivo + analisis_datos + model_arch)
 trainer.train(verbose = True, plot = True)
-#Test new model
+
 tester = Tester(model = model, 
                 testset = dataset.testset, 
                 batch_size = 8)
 tester.evaluate(plot = True)
-#Save new model
+
 model.save_model()
